96b0290d8a453b95b91118786280cb336387ab4f
Herman Christiansen <herman.chris@hotmail.com>
2020-10-10 01:48:48 +0200

    Updates

---
+++ precache-manifest.77721f068360df3bf7038daccda125dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ca3dfbbc93f32b16aba788a2c58627",
+    "url": "/ardoq/index.html"
+  },
+  {
+    "revision": "a62c8d9866121f412c99",
+    "url": "/ardoq/static/css/main.8e0c486e.chunk.css"
+  },
+  {
+    "revision": "58f547f967ee6ba5ad8c",
+    "url": "/ardoq/static/js/2.975f8f21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ardoq/static/js/2.975f8f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a62c8d9866121f412c99",
+    "url": "/ardoq/static/js/main.3cbaed48.chunk.js"
+  },
+  {
+    "revision": "2688d325d7f7db24f6ed",
+    "url": "/ardoq/static/js/runtime-main.e50dc6a5.js"
+  }
+]);
